50386fbc10e32eed710fb98bd730205a6fd8765f
Matt Clifford <matthewclifford@hotmail.co.uk>
2022-06-23 21:54:36 +0100

    update to admin page

--- routes/pages.js
+++ routes/pages.js
@@ -85,8 +85,15 @@ router.get('/contact', (req, res) => {
     res.render('contact')
 })
 
-router.get('/admin', (req, res) => {
-    res.render('admin')
+router.get('/admin', async (req, res) => {
+    const partners = await Partner.find()
+    res.render('admin', { partners: partners })
+})
+
+router.post('/edit', async (req, res) => {
+    const partnerId = req.body.partnerEdit
+    const partner = await Partner.findById(partnerId)
+    res.render('edit', { partner: partner })
 })
 
 module.exports = router
